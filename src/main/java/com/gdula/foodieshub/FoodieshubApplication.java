eae22e6cd768f0c211ab56952309e9fa872c2c67
Gdula <da.gdula@gmail.com>
2022-02-26 14:26:49 +0100

    Set dependencies, properties and connect to mySQL database

--- src/main/java/com/gdula/foodieshub/FoodieshubApplication.java
+++ src/main/java/com/gdula/foodieshub/FoodieshubApplication.java
@@ -2,8 +2,9 @@ package com.gdula.foodieshub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class }) //security disabled
 public class FoodieshubApplication {
 
 	public static void main(String[] args) {
