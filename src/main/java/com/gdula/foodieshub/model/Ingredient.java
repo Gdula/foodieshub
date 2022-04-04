b30efbc0813e466fc6bbd6267ef62b9b1f8ead43
Gdula <da.gdula@gmail.com>
2022-04-04 22:49:39 +0200

    Implement model for Recipe, Ingredient and User

---
+++ src/main/java/com/gdula/foodieshub/model/Ingredient.java
@@ -0,0 +1,29 @@
+package com.gdula.foodieshub.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ingredient {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Column(columnDefinition = "varchar(100)")
+    private String id;
+    @Size(min = 3)
+    private String name;
+    @NotNull
+    private String quantity;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Recipe recipe;
+}
---
+++ src/main/java/com/gdula/foodieshub/model/Recipe.java
@@ -0,0 +1,50 @@
+package com.gdula.foodieshub.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recipe {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Column(columnDefinition = "varchar(100)")
+    private String id;
+    @Size(min = 3)
+    private String name;
+    @NotNull
+    private Difficulty difficulty;
+    public static enum Difficulty {
+        Łatwe, Średnie, Trudne
+    }
+    @NotBlank
+    @Size(min = 3)
+    String description;
+    @NotNull
+    Integer minutesToPrepare;
+    @NotNull
+    private Category category;
+    public static enum Category {
+        Deser, Obiad, Kolacja, Przekąska
+    }
+    @Lob
+    private String image;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User owner;
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private List<Ingredient> ingredients;
+
+}
